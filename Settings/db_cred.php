0f3779968b9e802ed439e6f7d333225363303c82
eliz-kyera <87820310+eliz-kyera@users.noreply.github.com>
2022-10-22 21:27:03 +0000

    revamped

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -1,21 +1,20 @@
 <?php
 
-define('SERVER', 'us-cdbr-east-06.cleardb.net');
-define('USER', 'b2f9c7b34c2144');
-define('DATABASE', 'heroku_aaf43eb943b45e4');
-define('PASSWORD', "0883939f");
-
-
 // set up constants for database connection
 // database, user, password, server
 
-/*
 define('SERVER', 'localhost');
 define('USER', 'root');
 define('DATABASE', 'shoppn');
 define('PASSWORD', "");
+
+
+/*
+mysql://b2f9c7b34c2144:0883939f@us-cdbr-east-06.cleardb.net/heroku_aaf43eb943b45e4?reconnect=true
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USER', 'b2f9c7b34c2144');
+define('DATABASE', 'heroku_aaf43eb943b45e4');
+define('PASSWORD', "0883939f");
 */
-
-
 
 ?>
